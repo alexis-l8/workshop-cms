feb26703c2bb862b7bda4603e2ab3b0799f8d8d8
Alexis Lui <alexislui@hotmail.co.uk>
2017-03-13 15:17:51 +0000

    created handler function

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var http = require('http');
-var server = http.createServer();
+var server = http.createServer(handler);
+
+var message = "Alexis and Jessica are doing a sick job with this Node girls workshop";
+
+function handler (request, response){
+  response.writeHead(200, {"Content-Type" : "text/html"});
+  response.write(message);
+  response.end();
+}
 
 server.listen(3000, function(){
   console.log('Holla at port 3000. Ready to accept requests!');
